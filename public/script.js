4c6a4153a7ab8e8bf6b9e15f40f4a85bc8dc3360
Anton Kävenheim <akaevenheim@gmail.com>
2021-10-22 19:17:54 +0200

    finished sleeptimer, created external css file, restructured html, SSE now sends entire song metadata, not just image url

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 var serverEvents = new EventSource('http://localhost:5555/stream')
-const body = document.getElementById('changeMe')
-const msg = document.getElementById('heading')
+const body = document.querySelector('body')
+const msg = document.querySelector('h1')
+const image = document.getElementById('daImage')
 
 // show/hide cursor on user click
 body.addEventListener('click', (e) => {
@@ -13,75 +14,87 @@ body.addEventListener('click', (e) => {
 
 serverEvents.addEventListener('open', (event) => {
 		console.log('SSE Verbindung wurde erfolgreich hergestellt.');
-		// show friendly start up message:
-		setMsgText("HEOS Cover Art Server started!\n\n To start showing cover art, play or skip to a song.")
+		// show friendly welcome message:
+		setMsgText("HEOS COVER ART SERVER\n\nConnection successful!\nTo start showing cover art, play or skip to a song.")
 })
 
 serverEvents.onmessage = (event) => {
-	console.log("Event received!")
-	console.log('Event data: ' + event.data)
-	
-	body.style.backgroundImage = 'url(' + event.data + ')'
-	
-	// note: empty event.data isn't actually empty, it contains two quotes: ""
-	if (event.data == "\"\"" && !isSleeping && count == 0) {
-		console.log("Event contains no url, starting sleep timer...")
-		sleepTimer()
-	} else if (event.data == "\"\"" && !isSleeping && count != 0) {
-		console.log("Event contains no url, sleep timer continueing...")
-	} else if (event.data == "\"\"" && isSleeping) {
-		console.log("Event contains no url, staying asleep...")
-	} else if (!isSleeping && count == 0) {
-		console.log("Event contains valid url, updating cover art...")
-		setMsgText("")
-	} else if (!isSleeping && count != 0) {
-		console.log("Event contains valid url, sleep countdown interrupted.")
-		stopCounter()
-	} else if (isSleeping) {
-		console.log("Event contains valid url, waking up...")
+	console.log("Event received! Message:")
+	let message = JSON.parse(event.data)
+	console.log(message)
+
+	let isCounting = count != 0
+	let msgHasValidData = ('artist' in message) && message.artist != ''
+	let msgSaysStop = 'stopped' in message
+
+	// *** States/events matrix and desired results (see PDF for more info) ***
+	// Awake, non-counting client gets new album art, updates background:
+	if (!isSleeping && !isCounting && msgHasValidData) {
+		setImageSrc(message.image_url)
+		// clear welcome message:
+		setMsgText()
+	}
+	// Awake, non-counting client gets message that AVR turned off and starts sleeptimer:
+	if (!isSleeping && !isCounting && msgSaysStop) {
+		setImageSrc()
+		startTimer()
+	}
+	// Client counting down to sleep, but gets new album art, stops timer and updates background:
+	if (!isSleeping && isCounting && msgHasValidData) {
+		setImageSrc(message.image_url)
+		stopTimer()
+	}
+	// Sleeping client gets woken up by new album art:
+	if (isSleeping && !isCounting && msgHasValidData) {
+		setImageSrc(message.image_url)
 		wakeUp()
 	}
 }
 
-var count = 0
 var timer
 const secondsToSleep = 20
+var count = 0
+var remaining
 var isSleeping = false
 
-function sleepTimer() {
-	let countdown = secondsToSleep - count
-	console.log("Sleeping in... " + countdown)
-	setMsgText("Sleeping in... " + countdown)
+function startTimer() {
+	remaining = secondsToSleep - count
+	console.log("Sleeping in... " + remaining)
+	setMsgText("Sleeping in... " + remaining)
 	count++
 	if (count == secondsToSleep) {
 		sleep()
 	} else {
 		// with parantheses after timedCount, everything happens immediately and not once every second...
-		timer = setTimeout(sleepTimer, 1000)
+		timer = setTimeout(startTimer, 1000)
 	}
 }
-function stopCounter() {
+function stopTimer() {
+	console.log("Sleep timer stopped and reset.")
 	clearTimeout(timer)
 	count = 0
-	console.log("Sleep timer stopped and reset.")
-	if (!isSleeping) {
-		setMsgText("")
-		// body.innerHTML = ""
-	}
+	if (!isSleeping) setMsgText()
 }
 function sleep() {
-	isSleeping = true
 	console.log(`Counter has reached ${secondsToSleep} seconds, going to sleep now...`)
 	console.log("Sleeping... 😴")
-	setMsgText("zzzzz")
-	stopCounter()
+	isSleeping = true
+	setMsgText("😴")
+	count = 0
 }
 function wakeUp() {
-	isSleeping = false
 	console.log("Waking up...")
+	isSleeping = false
 	setMsgText("")
+	count = 0
 }
 
-function setMsgText(someText) {
+// default parameter value is empty string:
+function setMsgText(someText = '') {
 	msg.innerText = someText
+}
+
+function setImageSrc(imageUrl) {
+	// body.style.backgroundImage = `url(${imageUrl})`
+	image.style.backgroundImage = `url(${imageUrl})`
 }
--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const PORT = 5555
 const SSE = require('express-sse')
 const sse = new SSE()
 app.get('/stream', sse.init)
-var media
 
 HEOS = heos.discoverAndConnect()
 HEOS.then(connection => connection
 	.write('system', 'register_for_change_events', {enable: 'on'})
-	// .write('player', 'get_now_playing_media', {pid: 781222528})
+	.write('system', 'prettify_json_response', {enable: 'on'})
 	.on({	
 			commandGroup: 'event',
 			command: 'player_now_playing_changed'
@@ -27,11 +26,9 @@ HEOS.then(connection => connection
 		},
 		response => {
 			console.log(response.payload)
-			// Some actions in the spotify app cause erroneous now_playing change events with empty urls.
-			// The following code prevents these faulty events from triggering a server sent event.
-			if (response.payload.image_url != "") {
-				media = response.payload
-				sse.send(media.image_url)
+			// Some actions in the spotify app cause erroneous empty events being sent, preventing this with:
+			if (response.payload.artist != '') {
+				sse.send(response.payload)
 			}
 		}
 	)
@@ -42,18 +39,11 @@ HEOS.then(connection => connection
 		response => {
 			let state = response.heos.message.parsed.state
 			console.log(state)
-			if (state == 'stop') {
-				sse.send('')
-			}
+			if (state == 'stop') sse.send({stopped: 'stopped'})
 		}
 	)
 )
 
 app.use(express.static('public'))
-
-// app.get('/', (req, res) => {
-// 	res.set('Content-Type', 'text/html')
-// 	res.sendFile(__dirname + '/index.html')
-// })
 
 app.listen(PORT, () => console.log("Server is now listening to port %d.", PORT))
