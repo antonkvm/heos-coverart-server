6efc41b1384f70cbd473501e085658007f1deb3c
Anton Kävenheim <akaevenheim@gmail.com>
2021-10-23 11:29:46 +0200

    cover art update now generates new img elements with transition, still buggy tho

--- public/script.js
+++ public/script.js
@@ -1,7 +1,10 @@
 var serverEvents = new EventSource('http://localhost:5555/stream')
 const body = document.querySelector('body')
-const msg = document.querySelector('h1')
-const image = document.getElementById('daImage')
+const msgContainer = document.getElementById('messageContainer')
+const msgTitle = document.querySelector('h1')
+const msgBody = document.querySelector('p')
+var firstConnection = true
+var firstErrorSinceLastConnect = true
 
 // show/hide cursor on user click
 body.addEventListener('click', (e) => {
@@ -13,10 +16,27 @@ body.addEventListener('click', (e) => {
 })
 
 serverEvents.addEventListener('open', (event) => {
-		console.log('SSE Verbindung wurde erfolgreich hergestellt.');
-		// show friendly welcome message:
-		setMsgText("HEOS COVER ART SERVER\n\nConnection successful!\nTo start showing cover art, play or skip to a song.")
+	console.log('SSE Verbindung wurde erfolgreich hergestellt.');
+	// show friendly welcome message on first visit (only initial img elem won't have src attribute)
+	if (firstConnection) {
+		setMessageTitle("HEOS COVER ART SERVER")
+		setMessageBody("Connection successful!\nTo start showing cover art, play or skip to a song")
+	} else {
+		setMessageTitle("Reconnect successfull!")
+		setMessageBody("Play a new song to show cover art again.")
+	}
+	firstConnection = false
+	firstErrorSinceLastConnect = true
 })
+/** @todo Screen does not go to black on connection loss, fix pls. */
+serverEvents.onerror = (error) => {
+	setMessageTitle('Connection to server lost')
+	setMessageBody('Trying to reconnect...')
+	if (firstErrorSinceLastConnect) {
+		setImageUrl()
+		firstErrorSinceLastConnect= false
+	}
+}
 
 serverEvents.onmessage = (event) => {
 	console.log("Event received! Message:")
@@ -30,23 +50,24 @@ serverEvents.onmessage = (event) => {
 	// *** States/events matrix and desired results (see PDF for more info) ***
 	// Awake, non-counting client gets new album art, updates background:
 	if (!isSleeping && !isCounting && msgHasValidData) {
-		setImageSrc(message.image_url)
+		setImageUrl(message.image_url)
 		// clear welcome message:
-		setMsgText()
+		setMessageTitle()
+		setMessageBody()
 	}
 	// Awake, non-counting client gets message that AVR turned off and starts sleeptimer:
 	if (!isSleeping && !isCounting && msgSaysStop) {
-		setImageSrc()
+		setImageUrl()
 		startTimer()
 	}
 	// Client counting down to sleep, but gets new album art, stops timer and updates background:
 	if (!isSleeping && isCounting && msgHasValidData) {
-		setImageSrc(message.image_url)
+		setImageUrl(message.image_url)
 		stopTimer()
 	}
 	// Sleeping client gets woken up by new album art:
 	if (isSleeping && !isCounting && msgHasValidData) {
-		setImageSrc(message.image_url)
+		setImageUrl(message.image_url)
 		wakeUp()
 	}
 }
@@ -63,9 +84,10 @@ var remaining
 var isSleeping = false
 
 function startTimer() {
+	setImageUrl()
 	remaining = secondsToSleep - count
 	console.log("Sleeping in... " + remaining)
-	setMsgText("Sleeping in... " + remaining)
+	setMessageTitle("Sleeping in... " + remaining)
 	count++
 	if (count == secondsToSleep) {
 		sleep()
@@ -78,32 +100,66 @@ function stopTimer() {
 	console.log("Sleep timer stopped and reset.")
 	clearTimeout(timer)
 	count = 0
-	if (!isSleeping) setMsgText()
+	if (!isSleeping) setMessageTitle()
 }
 function sleep() {
 	console.log(`Counter has reached ${secondsToSleep} seconds, going to sleep now...`)
 	console.log("Sleeping... 😴")
 	isSleeping = true
-	setMsgText("😴")
+	setMessageTitle("😴")
 	count = 0
 }
 function wakeUp() {
 	console.log("Waking up...")
 	isSleeping = false
-	setMsgText("")
+	setMessageTitle("")
 	count = 0
 }
 
-/**
- * Manipulating the DOM
- * The functions below manipulate the DOM
- */
-
 // default parameter value is empty string:
-function setMsgText(someText = '') {
-	msg.innerText = someText
+function setMessageTitle(someText = '') {
+	msgTitle.innerText = someText
+}
+function setMessageBody(someText = '') {
+	msgBody.innerText = someText
 }
 
-function setImageSrc(imageUrl) {
-	image.style.backgroundImage = `url(${imageUrl})`
+/**
+ * Replaces the current cover art image with a new one.
+ * @param imageUrl The url to the new cover art image. Defaults to empty string.
+ * @todo If url param is empty, image should immediately go to black, instead of on next img iteration
+ */
+function setImageUrl(imageUrl = '') {
+
+	let oldElem = document.querySelector('img')
+	let newElem = document.createElement('img')
+	
+	// style new image, initially hidden by js-hide class:
+	newElem.setAttribute('src', imageUrl)
+	newElem.classList.add('js-hide')
+
+	// insert before message container:
+	body.insertBefore(newElem, msgContainer)
+ 
+	/**
+	 * Wait for new image to load, then start transition to reveal/hide new/old image.
+	 * I was to lazy to test if the load event actually fires, but it sure seems like it.
+	 */
+	newElem.onload = () => {
+		oldElem.classList.add('js-hide')
+		newElem.classList.remove('js-hide')
+
+		setTimeout(() => {
+			oldElem.remove()
+		}, 1000)
+	}
+	// flush unnecessary img elements if need be:
+	if (document.querySelectorAll('img').length > 2) {
+		let deletable = Array.from(document.querySelectorAll('img'))
+		deletable.pop()
+		deletable.map(node => node.remove())
+	}
+}
+function getImageUrl() {
+	return Array.from(document.querySelectorAll('img')).pop().getAttribute('src')
 }
