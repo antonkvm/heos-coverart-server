468ee78e6754557650c300e3ae8d8ab067ee1f86
Anton Kävenheim <akaevenheim@gmail.com>
2022-06-07 03:20:41 +0200

    bug fix: black sleep screen now disappears on wake

--- public/script.js
+++ public/script.js
@@ -36,6 +36,8 @@ $(()=>{
 			case 'new metadata':
 				updateScreen(data.payload)
 				$('#msg-title, #msg-body, #sleep1, #sleep2, #sleep3').text('')
+				$('#container').addClass('js-gradientBackdrop')
+				$('#container').css('background-color', '')
 				// The above without jquery:
 				// document.querySelectorAll('#msg-title, #msg-body, #sleep1, #sleep2, #sleep3').forEach((el)=>el.textContent='')
 				break
